f130f6865e4fda8b7c84493509dd03a0727bac28
WeirdestBoi <106311298+WeirdestBoi@users.noreply.github.com>
2022-06-04 16:54:40 +0200

    Add files via upload

---
+++ MiniGame/src/main/java/me/weirdestboi/minigame/MiniGame.java
@@ -0,0 +1,24 @@
+package me.weirdestboi.minigame;
+
+import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public final class MiniGame extends JavaPlugin {
+
+
+
+    public ArrayList<Player> Queue= new ArrayList<>();
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        getConfig().options().copyDefaults();
+        saveDefaultConfig();
+        getCommand("pvpgame").setExecutor(new startCommand(this));
+        }
+}
